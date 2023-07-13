0b742389ff477b4e9b09c4966423e86b4a7e24b6
KimKyounGs <77844251+KimKyounGs@users.noreply.github.com>
2023-07-13 22:40:42 +0900

    이니셜
    이니셜

---
+++ Source/GoodGame/GoodGameGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "GoodGameGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GOODGAME_API AGoodGameGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
