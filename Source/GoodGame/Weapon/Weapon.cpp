ab63e6b2142f0c791c5dc059ebc8cced68d2ce90
KimKyounGs <77844251+KimKyounGs@users.noreply.github.com>
2023-07-13 23:36:39 +0900

    에셋 추가
    에셋 추가

---
+++ Source/GoodGame/Weapon/Weapon.cpp
@@ -0,0 +1,49 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Weapon.h"
+#include "Components/SphereComponent.h"
+#include "Components/WidgetComponent.h"
+
+// Sets default values
+AWeapon::AWeapon()
+{
+	PrimaryActorTick.bCanEverTick = false;
+	bReplicates = true;
+
+	WeaponMesh = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("WeaponMesh"));
+	WeaponMesh->SetupAttachment(RootComponent);
+	SetRootComponent(WeaponMesh);
+
+	// 무기를 떨어뜨릴 때 사용.
+	// 그렇게 필요하지 않을 듯 하다.
+	WeaponMesh->SetCollisionResponseToAllChannels(ECollisionResponse::ECR_Block);
+	WeaponMesh->SetCollisionResponseToChannel(ECollisionChannel::ECC_Pawn, ECollisionResponse::ECR_Ignore); //이 코드는 "ECC_Pawn"이라는 충돌 채널에 대한 충돌 응답을 "ECR_Ignore"로 설정합니다.
+	WeaponMesh->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+
+	AreaSphere = CreateDefaultSubobject<USphereComponent>(TEXT("AreaSphere"));
+	AreaSphere->SetupAttachment(RootComponent); // 캐릭터와 무기의 겹침을 감지하기 위해서 사용.
+	AreaSphere->SetCollisionResponseToAllChannels(ECollisionResponse::ECR_Ignore);
+	AreaSphere->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+
+	//PickupWidget = CreateDefaultSubobject<UWidgetComponent>(TEXT("PickupWidget"));
+	//PickupWidget->SetupAttachment(RootComponent);
+}
+
+void AWeapon::BeginPlay()
+{
+	Super::BeginPlay();
+
+	// 서버에 있는 경우 -> 멀티플레이할 때 쓰는 것 일단 적어두고 주석처리
+	if (HasAuthority())
+	{
+		AreaSphere->SetCollisionEnabled(ECollisionEnabled::QueryAndPhysics);
+		AreaSphere->SetCollisionResponseToChannel(ECollisionChannel::ECC_Pawn, ECollisionResponse::ECR_Overlap);
+	}
+}
+
+void AWeapon::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
---
+++ Source/GoodGame/Weapon/Weapon.h
@@ -0,0 +1,45 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Weapon.generated.h"
+
+UENUM(BlueprintType)
+enum class EWeaponState : uint8
+{
+	EWS_Initial UMETA(DisplayName = "Initial State"), // 월드에 무기가 그대로 있을 때의 상태. 아직 아무도 집어들지 않았고 캐릭터가 집어 들고 장비할 수 있는 상태.
+	EWS_Equipped UMETA(DisplayName = "Equipped"), // 무기가 장비되어 있음.
+	EWS_Dropped UMETA(DisplayName = "Droppped"), // 무기를 가지고 있다가 떨어뜨렸을 때
+
+	EWS_MAX UMETA(DisplayName = "DefaultMAX") // 이렇게 정의된 최대값은 일밙거으로 범위 체크나 반복문에서 사용된다. 또한 열거형의 마지막 항목을 식별함.
+};
+
+UCLASS()
+class GOODGAME_API AWeapon : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	AWeapon();
+	virtual void Tick(float DeltaTime) override;
+
+protected:
+	virtual void BeginPlay() override;
+
+private:
+	UPROPERTY(VisibleAnywhere, Category = "Weapon Properties")
+		USkeletalMeshComponent* WeaponMesh;
+
+	UPROPERTY(VisibleAnywhere, Category = "Weapon Properties")
+		class USphereComponent* AreaSphere;
+
+	UPROPERTY(VisibleAnywhere, Category = "Weapon Properties")
+		EWeaponState WeaponState;
+
+	//UPROPERTY(VisibleAnywhere, Category = "Weapon Properties")
+	//class UWidgetComponent* PickupWidget;
+public:
+
+};
