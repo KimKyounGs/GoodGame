478e2c18e070037880d5b6e7c28346212458c230
KimKyounGs <77844251+KimKyounGs@users.noreply.github.com>
2023-08-16 22:34:44 +0900

    탄피
    탄피

--- Source/GoodGame/Weapon/Casing.h
+++ Source/GoodGame/Weapon/Casing.h
@@ -20,5 +20,12 @@ private:
 protected:
 	virtual void BeginPlay() override;
 
+	UFUNCTION()
+	virtual void OnHit(UPrimitiveComponent* HitComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit);
 
+	UPROPERTY(EditAnywhere)
+	float ShellEjectionImpulse;
+
+	UPROPERTY(EditAnywhere)
+	class USoundCue* ShellSound;
 };
