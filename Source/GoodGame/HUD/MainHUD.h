e6c6f99cfa01cd5aeb0fac9b80d870c6b00e1b93
MaEulHP <boy123753@gmail.com>
2023-08-20 01:27:10 +0900

    Revert "Merge branch 'main' of https://github.com/KimKyounGs/GoodGame"
    This reverts commit 8de7bd557905da07bfffcde00d0b3888e86623b7, reversing
    changes made to f142686a65235adee49245de5f21625048c49ce4.

--- Source/GoodGame/HUD/MainHUD.h
+++ Source/GoodGame/HUD/MainHUD.h
@@ -29,17 +29,6 @@ class GOODGAME_API AMainHUD : public AHUD
 	GENERATED_BODY()
 public:
 	virtual void DrawHUD() override;
-
-	UPROPERTY(EditAnywhere, Category = "Player Stats")
-	TSubclassOf<class UUserWidget> CharacterOverlayClass;
-
-	class UCharacterOverlay* CharacterOverlay;
-
-protected:
-	virtual void BeginPlay() override;
-	void AddCharacterOverlay();
-
-
 private:
 	FHUDPackage HUDPackage;
 
