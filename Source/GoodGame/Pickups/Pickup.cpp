5d74eeceda29bd00a4d9db1cbd441f9b2167da63
KimKyounGs <77844251+KimKyounGs@users.noreply.github.com>
2023-08-28 23:39:06 +0900

    SpawnPickup
    SpawnPickup

--- Source/GoodGame/Pickups/Pickup.cpp
+++ Source/GoodGame/Pickups/Pickup.cpp
@@ -40,8 +40,18 @@ void APickup::BeginPlay()
 	Super::BeginPlay();
 
 	
-	OverlapSphere->OnComponentBeginOverlap.AddDynamic(this, &APickup::OnSphereOverlap);
+	GetWorldTimerManager().SetTimer(
+		BindOverlapTimer,
+		this,
+		&APickup::BindOverlapTimerFinished,
+		BindOverlapTime
+	);
 	
+}
+
+void APickup::BindOverlapTimerFinished()
+{
+	OverlapSphere->OnComponentBeginOverlap.AddDynamic(this, &APickup::OnSphereOverlap);
 }
 
 void APickup::OnSphereOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
